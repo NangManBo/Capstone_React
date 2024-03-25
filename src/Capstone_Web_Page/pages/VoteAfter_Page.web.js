1e1af72f07b2451c20f06a76a6bd8e1b0872f97c
NanaManBo <kimbosung22@naver.com>
2024-03-25 17:27:39 +0900

    vote페이지 navigate, location 확인

--- src/Capstone_Web_Page/pages/VoteAfter_Page.web.js
+++ src/Capstone_Web_Page/pages/VoteAfter_Page.web.js
@@ -1,6 +1,44 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 function voteAfterPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {
+    isLoggedIn,
+    userId,
+    vote,
+    jwtToken,
+    nickname,
+    category,
+    userVotes,
+  } = location.state || {};
+  useEffect(() => {
+    console.log('vote', vote);
+    console.log('userVotes', userVotes);
+    console.log('category', category);
+    console.log('isLoggedIn', isLoggedIn);
+    console.log('userId', userId);
+    console.log('jwtToken', jwtToken);
+    console.log('nickname', nickname);
+  }, []);
   return (
     <div>
+      <button
+        onClick={() =>
+          navigate('/main', {
+            state: {
+              isLoggedIn,
+              userId,
+              jwtToken,
+              nickname,
+            },
+          })
+        }
+      >
+        뒤로가기
+      </button>
       <h1>Vote After Page</h1>
     </div>
   );
--- src/Capstone_Web_Page/pages/VoteBefore_Page.web.js
+++ src/Capstone_Web_Page/pages/VoteBefore_Page.web.js
@@ -1,7 +1,46 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 function voteBeforePage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const {
+    isLoggedIn,
+    userId,
+    vote,
+    jwtToken,
+    nickname,
+    category,
+    userVotes,
+  } = location.state || {};
+  useEffect(() => {
+    console.log('vote', vote);
+    console.log('userVotes', userVotes);
+    console.log('category', category);
+    console.log('isLoggedIn', isLoggedIn);
+    console.log('userId', userId);
+    console.log('jwtToken', jwtToken);
+    console.log('nickname', nickname);
+  }, []);
   return (
     <div>
-      <h1>Vote After Page</h1>
+      <button
+        onClick={() =>
+          navigate('/main', {
+            state: {
+              isLoggedIn,
+              userId,
+              jwtToken,
+              nickname,
+            },
+          })
+        }
+      >
+        뒤로가기
+      </button>
+      <h1>Vote Before Page</h1>
+      <button></button>
     </div>
   );
 }
