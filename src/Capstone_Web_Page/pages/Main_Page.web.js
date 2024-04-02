1d247c4fe13fe24f925403d0d41e4f19a88eb3d2
NanaManBo <kimbosung22@naver.com>
2024-04-03 02:15:58 +0900

    🐛

--- src/Capstone_Web_Page/pages/Main_Page.web.js
+++ src/Capstone_Web_Page/pages/Main_Page.web.js
@@ -133,9 +133,7 @@ function MainPage() {
       );
     }
   };
-  useEffect(() => {
-    fetchVotes(jwtToken, setSearchResults);
-  }, [searchQuery]);
+
   // 투표 데이터를 받아오는 함수
   useEffect(() => {
     fetchVotes(setVotes, jwtToken);
