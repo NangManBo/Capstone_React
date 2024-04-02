65560c96e6d848e3ca64aae14f24ac7c43015c55
NanaManBo <kimbosung22@naver.com>
2024-04-03 02:41:22 +0900

    🐛

--- src/Capstone_Web_Page/pages/Main_Page.web.js
+++ src/Capstone_Web_Page/pages/Main_Page.web.js
@@ -134,7 +134,124 @@ function MainPage() {
       );
     }
   };
+  const renderPostPress = async (firstMatchingVote) => {
+    try {
+      // Fetch user votes from the backend
+      const response = await axios.get(
+        'https://port-0-capstone-project-gj8u2llon19kg3.sel5.cloudtype.app/votes/ok/' +
+          nickname,
+        {
+          headers: {
+            'AUTH-TOKEN': jwtToken,
+          },
+        }
+      );
 
+      if (response.status === 200) {
+        //console.log('내가 투표한 데이터', response.data);
+        const userVotes = response.data;
+        console.log('userVotes:', userVotes);
+
+        // Check if userVotes is null or empty
+        if (!userVotes || userVotes.length === 0) {
+          const isCreatedByUser =
+            firstMatchingVote.createdBy === nickname;
+
+          // Check if the vote is closed
+          const isVoteEnd =
+            firstMatchingVote.voteStatus === 'CLOSED';
+          console.log('first' + firstMatchingVote);
+          if (nickname === 'manager') {
+            navigate('/voteonlylook', {
+              state: {
+                vote: firstMatchingVote,
+              },
+            });
+          }
+          // Navigate to the appropriate screen based on voting status, createdBy, and voteStatus
+          else if (isVoteEnd) {
+            // If the vote is closed, navigate to 'VoteEnd'
+            navigate('/voteend', {
+              state: {
+                vote: firstMatchingVote,
+                isLoggedIn,
+                userId,
+                jwtToken,
+                nickname,
+                userVotes,
+              },
+            });
+          } else if (isCreatedByUser) {
+            // If the user created the vote, navigate to 'VoteCreatedUser'
+            navigate('/votecreateduser', {
+              state: {
+                vote: firstMatchingVote,
+                isLoggedIn,
+                userId,
+                jwtToken,
+                nickname,
+                userVotes,
+              },
+            });
+          } else {
+            // If userVotes is null or empty, navigate to 'VoteBefore'
+            navigate('/votebefore', {
+              state: {
+                vote: firstMatchingVote,
+                isLoggedIn,
+                userId,
+                jwtToken,
+                nickname,
+              },
+            });
+          }
+        } else {
+          // Continue with the existing logic for non-empty userVotes
+          const isCreatedByUser =
+            firstMatchingVote.createdBy === nickname;
+
+          // Check if the vote is closed
+          const isVoteEnd =
+            firstMatchingVote.voteStatus === 'CLOSED';
+          // Ensure vote.category is defined
+          const category = firstMatchingVote.category || '';
+          // Check if the user has voted for the selected poll
+          const hasVoted = userVotes.some(
+            (userVote) =>
+              userVote.pollId === firstMatchingVote.id
+          );
+          // Navigate to the appropriate screen based on voting status, createdBy, and voteStatus
+          navigate(
+            isVoteEnd
+              ? '/voteend'
+              : isCreatedByUser
+              ? '/votecreateduser'
+              : hasVoted
+              ? '/voteafter'
+              : '/votebefore',
+            {
+              state: {
+                category,
+                vote: firstMatchingVote,
+                isLoggedIn,
+                userId,
+                jwtToken,
+                nickname,
+                userVotes,
+              },
+            }
+          );
+        }
+      } else {
+        console.error(
+          '투표 들어가려는데 실패:',
+          response.data
+        );
+      }
+    } catch (error) {
+      console.error('투표 들어가려는데 오류:', error);
+    }
+  };
   // 투표 데이터를 받아오는 함수
   useEffect(() => {
     fetchVotes(setVotes, jwtToken);
@@ -202,7 +319,9 @@ function MainPage() {
                     <div
                       key={result.id}
                       className="search-result-view3"
-                      //onClick={() => renderPostPress(result)}
+                      onClick={() =>
+                        renderPostPress(result)
+                      }
                     >
                       <span className="search-result-title">
                         {JSON.parse(result.title).title}
