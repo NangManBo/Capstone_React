35f6f26663c5f23c12cf1aa658db803cc5dbf70c
NanaManBo <kimbosung22@naver.com>
2024-04-03 02:12:50 +0900

    검색 테스트

--- src/Capstone_Web_Page/pages/Main_Page.web.js
+++ src/Capstone_Web_Page/pages/Main_Page.web.js
@@ -123,7 +123,7 @@ function MainPage() {
   };
   const handleSearch = () => {
     if (searchQuery.length >= 2) {
-      fetchVotes(setVotes, jwtToken, setSearchResults);
+      fetchVotes(jwtToken, setSearchResults);
     } else {
       alert(
         '검색 오류',
