2bd673cb7757970280729bce825a11499d0f81f9
NanaManBo <kimbosung22@naver.com>
2024-04-10 20:34:01 +0900

    🐛

--- src/Capstone_Web_Page/pages/VoteOnlyLook.web.js
+++ src/Capstone_Web_Page/pages/VoteOnlyLook.web.js
@@ -59,18 +59,10 @@ function VoteOnlyLookPage() {
         <span>뒤로가기</span>
       </button>
       <div>
-        <h1>
-          {vote &&
-            vote.title &&
-            JSON.parse(vote.title).title}
-        </h1>
+        <h1>{vote && vote.title}</h1>
         <p>투표 기간 설정: {vote && vote.createdAt}</p>
         <p>주최자 : {vote && vote.createdBy}</p>
-        <p>
-          {vote &&
-            vote.question &&
-            JSON.parse(vote.question).question}
-        </p>
+        <p>{vote && vote.question}</p>
         <p>
           사진 url : <span>{vote.mediaUrl}</span>
         </p>
