7f0e19e0ee6f5654a01137bc47f85b87b11aa62b
NanaManBo <kimbosung22@naver.com>
2024-06-10 00:49:53 +0900

    수정~

--- src/Capstone_Web_Page/components/popularVoteBanner_components.js
+++ src/Capstone_Web_Page/components/popularVoteBanner_components.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/poplularBanner_style.css';
+import './styles/popularBanner_style.css';
 import { renderPostPress } from '../functions/renderPostPress_function';
 
 const fillEmptyVotes = (votes, count) => {
