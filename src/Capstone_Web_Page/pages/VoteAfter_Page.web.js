a7f3b51001340a4d21aba90f7c76e26b31c9b39d
NanaManBo <kimbosung22@naver.com>
2024-06-10 22:48:28 +0900

    🐛

--- src/Capstone_Web_Page/pages/VoteAfter_Page.web.js
+++ src/Capstone_Web_Page/pages/VoteAfter_Page.web.js
@@ -602,9 +602,19 @@ function VoteAfterPage() {
       let formData = new FormData();
       const parentCommentId = replyingIndex; // Get the parent comment ID
 
+      // @를 포함한 userNickname을 제외하고 싶은 문자열
+      const mention = `@${comment.userNickname} `;
+
+      // 원본 replyText에서 mention을 제거한 텍스트
+      const contentWithoutMention = replyText.replace(
+        mention,
+        ''
+      );
+
+      // 서버로 contentWithoutMention를 보내기
       formData.append(
         'content',
-        JSON.stringify({ content: replyText })
+        JSON.stringify({ content: contentWithoutMention })
       );
       if (selectedMedia) {
         const localUri = selectedMedia;
